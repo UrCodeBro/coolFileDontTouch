18c9097f2821240de2dbd493bbc9d09b0e4bb58a
UrCodeBro <33433978+UrCodeBro@users.noreply.github.com>
2017-11-08 15:50:24 -0600

    Update type.js

--- testFolderPleaseIgnore/Code/type.js
+++ testFolderPleaseIgnore/Code/type.js
@@ -23,6 +23,6 @@ async function myFunction() {
   document.getElementById("cant").display = "block";
 	document.getElementById("signOut").display = "block";
 	console.log(getElementById("cant").display);
-	await sleep(1000);
+	await sleep(10000);
 	window.location.assign("home.html");
 }
